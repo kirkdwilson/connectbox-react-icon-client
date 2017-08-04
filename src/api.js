096a61384815b6d27cbc64ddd7ab3ecd58047a7b
Kelly Davis <kldavis4@gmail.com>
2017-08-04 17:32:11 -0400

    Modify to only provide a default value for the popular files - content and config should always be present and if not should   return an error

--- src/api.js
+++ src/api.js
@@ -1,19 +1,22 @@
 import axios from 'axios'
 
 export function getContent (contentPath) {
-  return get(`/content/${contentPath}/`, [])
+  return get(`/content/${contentPath}/`)
 }
 
 export function getConfig (configPath) {
-  return get(configPath, {})
+  return get(configPath)
 }
 
 export function getStats (statsPath) {
   return get(statsPath, {})
 }
 
-function get (url, val) {
+function get (url, defaultValue) {
   return axios.get(url).then(resp => resp.data).catch((e) => {
-    return val
+    if (defaultValue) {
+      return defaultValue
+    }
+    throw e
   })
 }
